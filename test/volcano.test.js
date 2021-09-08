dc8c938bdbb53655510401f8db02df248ff46011
Chong Kai En <kaien97@gmail.com>
2021-09-08 17:39:59 +0100

    updated tests to use .call() for view functions

--- test/volcano.test.js
+++ test/volcano.test.js
@@ -41,8 +41,8 @@ contract("VolcanoToken", accounts => {
     it("should start with no tokens", async () => {
     const instance = await VolcanoToken.deployed();
     //console.log(instance);
-    const owner = await instance.owner();
-    const ownerBal = await instance.balanceOf(owner);
+    const owner = await instance.owner.call();
+    const ownerBal = await instance.balanceOf.call(owner);
     assert.equal(
         ownerBal.toNumber(),
        0,
@@ -53,9 +53,9 @@ contract("VolcanoToken", accounts => {
     it("minting should give 1 token", async () => {
     const instance = await VolcanoToken.deployed();
     //console.log(instance);
-    const owner = await instance.owner();
+    const owner = await instance.owner.call();
     await instance.mint({from: owner});
-    const ownerBal = await instance.balanceOf(owner);
+    const ownerBal = await instance.balanceOf.call(owner);
     assert.equal(
         ownerBal.toNumber(),
        1,
@@ -66,7 +66,7 @@ contract("VolcanoToken", accounts => {
     it("addresses should initialize with 0 balance", async () => {
       const instance = await VolcanoToken.deployed();
       const recipient = "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4";
-      const recipBal = await instance.balanceOf(recipient);
+      const recipBal = await instance.balanceOf.call(recipient);
       assert.equal(
           recipBal.toNumber(),
          0,
@@ -76,12 +76,12 @@ contract("VolcanoToken", accounts => {
 
     it("tokens can be transferred by owner", async () => {
     const instance = await VolcanoToken.deployed();
-    const owner = await instance.owner();
+    const owner = await instance.owner.call();
     const recipient = (await web3.eth.getAccounts())[1];
     //await instance.mint({from: owner});
     await instance.safeTransferFrom(owner, recipient, 0);
-    const ownerBal = await instance.balanceOf(owner);
-    const recipBal = await instance.balanceOf(recipient);
+    const ownerBal = await instance.balanceOf.call(owner);
+    const recipBal = await instance.balanceOf.call(recipient);
     assert.equal(
         ownerBal.toNumber(),
        0,
@@ -111,7 +111,7 @@ contract("VolcanoToken", accounts => {
     const instance = await VolcanoToken.deployed();
     const tokenOwner = await instance.ownerOf(0);
     await instance.burn(0, {from: tokenOwner});
-    const recipBal = await instance.balanceOf(tokenOwner);
+    const recipBal = await instance.balanceOf.call(tokenOwner);
 
     assert.equal(
         recipBal.toNumber(),
